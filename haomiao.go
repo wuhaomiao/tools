282b4d96c29f7087c43dd49d264c56afa78c6d01
cat <cat@catdeMac.local>
2021-06-18 17:23:55 +0800

    增加用户输入方法

--- haomiao.go
+++ haomiao.go
@@ -1,7 +1,11 @@
 package haomiao
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,6 +14,36 @@ func GetDate() string {
 	now := time.Now()
 	nowTime := fmt.Sprintln(now.Format("2006-01-02 15:04:05"))
 	return nowTime
+}
+
+// 获取用户输入字符串
+func inputStr(info string) (text string) {
+	fmt.Print(info)
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("读取有误: ", err)
+		return
+	}
+	text = strings.TrimSpace(text)
+	return text
+}
+
+// 获取用户输入数字
+func inputInt(info string) (num int) {
+	fmt.Print(info)
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("读取有误: ", err)
+		return
+	}
+	text = strings.TrimSpace(text)
+	num, err = strconv.Atoi(text)
+	if err != nil {
+		fmt.Println("转换为Int类型出错：", err)
+	}
+	return num
 }
 
 // Line 打印分隔线
